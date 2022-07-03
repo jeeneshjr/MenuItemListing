e231808a44f6b4aa00d2ac99ada6f70546bc1a3d
jeeneshjr <jeenejr2k30@gmail.com>
2022-07-03 13:22:25 +0530

    Add project files.

---
+++ MenuItemListing/Controllers/MenuItemController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MenuItemListing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuItemController : ControllerBase
+    {
+       public static List<MenuItem> MenuList = new List<MenuItem>()
+            {   new MenuItem() {
+                Id=1, 
+                Name="Keyboard", 
+                Active=true, 
+                DateOfLaunch=new DateTime(2022,06,01), 
+                FreeDelivery=false,Price=1500},
+
+                new MenuItem() {
+                    Id=2, 
+                    Name="Mouse", 
+                    Active=false, 
+                    DateOfLaunch=new DateTime(2022,06,03), 
+                    FreeDelivery=false,Price=800}
+
+            };
+
+        [HttpGet]
+        public List<MenuItem> GetItem()
+        {
+            
+            return MenuList;
+        }
+
+        [HttpGet("{id}")]
+        public MenuItem GetItemById(int id)
+        {
+            MenuItem obj = MenuList.SingleOrDefault(item => item.Id == id);
+            return obj;
+        }
+    }
+}
